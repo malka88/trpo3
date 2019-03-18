bb534fa685d18d05fab25e9b6dcc026753dae731
Malka Schildt <malka_88@outlook.com>
2019-03-18 14:59:10 +0700

    sub

--- trpo3/Program.cs
+++ trpo3/Program.cs
@@ -8,6 +8,14 @@ namespace trpo3
 {
     class Program
     {
+        static void sub(float a, float b)
+        {
+            Console.Clear();
+            float x = a - b;
+            Console.WriteLine("{1}", x);
+            Console.ReadKey();
+        }
+
         static void Main(string[] args)
         {
             float a, b;
