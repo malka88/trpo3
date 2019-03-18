3560e7cc89e3b66c9ba5c9bfd365d0ab4b021c52
Malka Schildt <malka_88@outlook.com>
2019-03-18 14:54:54 +0700

    main

--- trpo3/Program.cs
+++ trpo3/Program.cs
@@ -10,6 +10,32 @@ namespace trpo3
     {
         static void Main(string[] args)
         {
+            float a, b;
+            int x;
+            Console.WriteLine("Enter A and B: ");
+            a = float.Parse(Console.ReadLine());
+            b = float.Parse(Console.ReadLine());
+            Console.WriteLine("Choose the operation:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division");
+            x = int.Parse(Console.ReadLine());
+
+            switch (x)
+            {
+                case 1:
+                    add(a, b);
+                    break;
+                case 2:
+                    sub(a, b);
+                    break;
+                case 3:
+                    mul(a, b);
+                    break;
+                case 4:
+                    div(a, b);
+                    break;
+                default:
+                    break;
+            }
         }
+    }
     }
 }
